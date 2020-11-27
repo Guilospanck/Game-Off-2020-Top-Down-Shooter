0ff7eb392f468843e99eecfa4560a2ce1e5dd2a5
guilospanck <guilherme.rp2014@gmail.com>
2020-11-27 19:39:48 -0300

    adding files

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float speed;
+    public float timeBetweenAttacks;
+    public int damage;
+
+    public int healthPickupChance;
+    public GameObject healthPickup;
+    public int pickupChance;
+    public GameObject[] pickups;
+
+    public GameObject enemyDeathEffect;
+
+    
+    [HideInInspector]
+    public Transform player; // We'll want the enemies to follow the player
+
+    public virtual void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void TakeDamage(int damage) {
+        health -= damage;
+
+        if(health <= 0) {
+            int randomNumber = Random.Range(0, 101);
+            if(randomNumber < pickupChance){
+                GameObject pickup = pickups[Random.Range(0, pickups.Length)];
+                Instantiate(pickup, transform.position, transform.rotation);
+            }
+
+            if(randomNumber < healthPickupChance){
+                Instantiate(healthPickup, transform.position, transform.rotation);
+            }
+            
+            Instantiate(enemyDeathEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public float speed;
+    public float lifetime;
+    public int damage;
+
+    public GameObject soundEffect;
+
+    public GameObject explosion;
+
+    void Awake()
+    {
+        Instantiate(soundEffect, transform.position, transform.rotation);
+    }
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifetime);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.up * Time.deltaTime * speed);
+    }
+
+    void DestroyProjectile(){
+        Instantiate(explosion, transform.position, Quaternion.identity); // instantiates the explosion particle
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.tag == "Enemy") {
+            collision.GetComponent<Enemy>().TakeDamage(damage);
+            DestroyProjectile();
+        }
+
+        if (collision.tag == "Boss")
+        {
+            collision.GetComponent<Boss>().TakeDamage(damage);
+            DestroyProjectile();
+        }
+    }
+}
